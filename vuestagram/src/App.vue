5e4652ab070e46d6a23e31c05da8972855b61108
zosaekki <chohyunm1n@naver.com>
2022-09-22 02:33:29 +0900

    vuestagram ajax, axios

---
+++ vuestagram/src/App.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <Container :postData="postData" />
+  <button @click="more">더보기</button>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+
+  <div v-if="tab == 0">내용0</div>
+  <div v-if="tab == 1">내용1</div>
+  <div v-if="tab == 2">내용2</div>
+
+  <button @click="tab = 0">버튼0</button>
+  <button @click="tab = 1">버튼1</button>
+  <button @click="tab = 2">버튼2</button>
+</template>
+
+<script>
+import Container from './components/Container.vue';
+import Data from './assets/data';
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+    Container,
+  },
+  data() {
+    return {
+      postData: Data,
+      moreBtnClick: 0,
+      tab: 0
+    }
+  },
+  methods: {
+    more() {
+      axios.get(`https://codingapple1.github.io/vue/more${this.moreBtnClick}.json`)
+        .then(res => {
+          this.postData.push(res.data);
+          this.moreBtnClick++;
+        })
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+
+.inputfile {
+  display: none;
+}
+
+.input-plus {
+  cursor: pointer;
+}
+
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+</style>
---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post :postData="postData" />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    name: 'Container',
+    components: {
+      Post,
+    },
+    props: {
+      postData: Array
+    }
+}
+</script>
+
+<style>
+
+</style>
